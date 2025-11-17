098f2d5068834572d5129725ad1af6c3dbd93738
Matias Jørgensen <matjorrub@gmail.com>
2025-11-17 13:33:08 +0100

    Add SVG icons for React and User, and implement Header component structure
    - Added react.svg icon to assets
    - Added user.svg icon to assets
    - Created Header component with basic structure including an image, a dropdown for location selection, and a button

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header(){
+    return(
+        <header>
+            <figure>
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <figcaption>
+                   <select>
+                    <option value="New York">New York</option>
+                   </select>
+                </figcaption>
+            </figure>
+            <button></button>
+        </header>
+    )
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,30 @@
-export default function(){
-    
+import { NavLink } from "react-router";
+
+export default function () {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">
+            <img src="" alt="" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/">
+            <img src="" alt="" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/">
+            <img src="" alt="" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/">
+            <img src="" alt="" />
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
